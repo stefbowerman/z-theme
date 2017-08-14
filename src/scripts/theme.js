e98591b0ff25052cbd355c0e1b96a68eb6516f14
Justin Mather <justin.mather@gmail.com>
2017-08-14 14:59:55 -0700

    Import filters from BF

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -18,6 +18,7 @@ window.theme = window.theme || {};
 /*================ Templates ================*/
 // =require templates/customers-addresses.js
 // =require templates/customers-login.js
+// =require templates/filters.js
 
 $(document).ready(function() {
   var sections = new slate.Sections();
